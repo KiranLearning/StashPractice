b2f4e3a2763459ade91d2554373a3e648e46ec8f
kiran <Kiran.Halagi@octilyon.com>
2024-05-17 00:25:01 +0530

    FirstStash Practice

---
+++ src/main/java/stashPractice/AddCardPage.java
@@ -0,0 +1,13 @@
+package stashPractice;
+
+public class AddCardPage {
+
+	public static void main(String[] args) {
+		addCardPage();
+
+	}
+
+	public static void addCardPage() {
+		System.out.println("This code is created");
+	}
+}
