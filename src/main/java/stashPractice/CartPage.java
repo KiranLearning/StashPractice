8366947eed4958ac1a382d2891b04f976869c0d7
KiranLearning <131030328+KiranLearning@users.noreply.github.com>
2024-05-17 00:40:08 +0530

    Created CartPage.java

---
+++ src/main/java/stashPractice/CartPage.java
@@ -0,0 +1,14 @@
+package stashPractice;
+
+public class CartPage {
+
+	public static void main(String[] args) {
+		cartPageDevelopement();
+		
+	}
+	
+	public static void cartPageDevelopement() {
+		System.out.println("Developed By Dev2");
+	}
+
+}
