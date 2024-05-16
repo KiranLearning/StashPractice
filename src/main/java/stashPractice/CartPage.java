4eb49f3f90248ad508b5a368c773dfd607ca10a6
kiran <Kiran.Halagi@octilyon.com>
2024-05-17 01:14:38 +0530

    Solved merge conflict

--- src/main/java/stashPractice/CartPage.java
+++ src/main/java/stashPractice/CartPage.java
@@ -3,8 +3,13 @@ package stashPractice;
 public class CartPage {
 
 	public  void cart() {
+<<<<<<< Updated upstream
 		String nameD2 = "cart Dev2";
 		System.out.println(nameD2);
+=======
+		String nameD1 = "Cart D1";
+		System.out.println(nameD1); 
+>>>>>>> Stashed changes
 	}
 
 }
